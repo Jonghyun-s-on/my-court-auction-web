24e040dccd65123550fb0faab61fa9689c0f602d
jonghyun <dev.son.jh.@gmail.com>
2025-07-10 17:41:58 +0900

    더미 데이터 및 API 테스트 페이지 추가

--- routes/api/lands.py
+++ routes/api/lands.py
@@ -2,13 +2,8 @@ from flask import Blueprint, jsonify
 
 lands_bp = Blueprint('lands', __name__)
 
-lands = [
-    {"id": 1, "name": "토지 A", "lat": 37.655943, "lng": 126.840551},
-    {"id": 2, "name": "토지 B", "lat": 37.654321, "lng": 126.841111},
-    {"id": 3, "name": "토지 C", "lat": 37.656789, "lng": 126.842222}
-]
 
-
-@lands_bp.route('/lands')
+@lands_bp.route('/lands', methods=['GET'])
 def get_lands():
-    return jsonify(lands)
+    temp_lands_info = {"id": 1, "no": "A153", "name": "토지 A", "lat": 37.655943, "lng": 126.840551}
+    return jsonify(temp_lands_info)
--- routes/views.py
+++ routes/views.py
@@ -1,9 +1,15 @@
+# 웹 브라우저 주소창을 통해 요청이 왔을 때, 해당 화면(HTML)을 응답해주는 역할
 from flask import Blueprint, render_template, current_app
 
 views_bp = Blueprint('views', __name__)
 
-
-@views_bp.route('/')
+@views_bp.route('/index')
 def index():
     kakao_key = current_app.config['KAKAO_MAP_KEY']
     return render_template('index.html', kakao_key=kakao_key)
+
+# 테스트 페이지
+# 추후 삭제 예정
+@views_bp.route('/test')
+def test():
+    return render_template('test.html')
